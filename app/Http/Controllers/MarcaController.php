2926c9091fa0408c4bc9ad1d346cbde280539068
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2022-10-04 14:53:40 -0300

    Update

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -34,7 +34,9 @@ class MarcaController extends Controller
 
     public function update(Request $request, Marca $marca)
     {
-        //
+        $marca->update($request->all());
+        
+        return $marca;
     }
 
 
