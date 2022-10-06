14c787f0e12d1b1fa7a097d2df3016ac37f28856
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2022-10-06 19:00:38 -0300

    group

--- routes/api.php
+++ routes/api.php
@@ -23,12 +23,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::apiResource('cliente', ClienteController::class);
-Route::apiResource('carro', CarroController::class);
-Route::apiResource('locacao', LocacaoController::class);
-Route::apiResource('marca', MarcaController::class);
-Route::apiResource('modelo', ModeloController::class);
+Route::prefix('v1')->middleware('jwt.auth')->group(function(){
+    Route::apiResource('cliente', ClienteController::class);
+    Route::apiResource('carro', CarroController::class);
+    Route::apiResource('locacao', LocacaoController::class);
+    Route::apiResource('marca', MarcaController::class);
+    Route::apiResource('modelo', ModeloController::class);
+});
 
 
 Route::post('login', [AuthController::class, 'login']);
